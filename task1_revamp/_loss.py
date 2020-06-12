e30ad076543177d7624bf50919daa61bbb87c84d
Patrick Yang <patrick22414@outlook.com>
2020-06-12 23:59:51 +0800

    Task 1 CTPN WIP

---
+++ task1_revamp/_loss.py
@@ -0,0 +1,14 @@
+import torch
+from torch.nn import CrossEntropyLoss, SmoothL1Loss
+
+
+def loss_classification():
+    pass
+
+
+def loss_vertical_coordinates():
+    pass
+
+
+def loss_side_refinement():
+    pass
