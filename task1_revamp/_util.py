27be4271b251c256f695acbade9a801bffe85994
Patrick Yang <patrick22414@outlook.com>
2020-07-21 01:09:30 +0800

    Task 1 NMS stuff

--- task1_revamp/_util.py
+++ task1_revamp/_util.py
@@ -24,51 +24,94 @@ def outputs_to_boxes(
 
     # transform y_2 from [v_c, v_h] to [c_y, h]
     center_y_of_anchors = (torch.arange(y_2.size(1)) * 16 + 8).view(1, -1, 1, 1)
+
     y_2[..., 0].mul_(anchors).add_(center_y_of_anchors)
     y_2[..., 1].exp_().mul_(anchors)
 
+    # TODO: delete anchors with a text score below threshold
+    # wait, can we do that?
 
-def _vertical_nms(text_scores: Tensor, coordinates: Tensor):
+
+def _vertical_nms(
+    text_scores: Tensor,
+    coordinates: Tensor,
+    confidence_threshold=0.75,
+    suppression_threshold=0.5,
+):
     """Apply NMS to each column of anchors.
 
     Inputs:
-        text_scores: [N x H x k]
-        coordinates: [N x H x k x 2]
-    """
-    pass
-
-
-def _iou(box_1, box_2):
-    """Intersection Over Union
-
-    Input:
-        box_1 -- a sequence like [x_0, y_0, x_1, y_1], representing top-left and bottom-right corners of a box
-        box_2 -- same as box_1
+        text_scores: [Na]
+        coordinates: [Na x 2]
     """
 
-    min_x_0 = min(box_1[0], box_2[0])
-    min_y_0 = min(box_1[1], box_2[1])
-    min_x_1 = min(box_1[2], box_2[2])
-    min_y_1 = min(box_1[3], box_2[3])
+    is_active = torch.ones_like(text_scores, dtype=torch.bool)
+    is_active[text_scores < confidence_threshold] = False
 
-    max_x_0 = max(box_1[0], box_2[0])
-    max_y_0 = max(box_1[1], box_2[1])
-    max_x_1 = max(box_1[2], box_2[2])
-    max_y_1 = max(box_1[3], box_2[3])
+    indices = torch.argsort(text_scores)
 
-    area_i = (min_x_1 - max_x_0) * (min_y_1 - max_y_0)
-    area_o = (max_x_1 - min_x_0) * (max_y_1 - min_y_0)
+    for i in indices:
+        if not is_active[i]:
+            continue
+
+        iou = _vertical_iou(coordinates[i], coordinates)
+
+        is_active[iou > suppression_threshold] = False
+        is_active[i] = True
+
+    return is_active
+
+
+def _vertical_iou(anchor_0: Tensor, anchors: Tensor):
+    """Intersection Over Union for vertical anchors
+    """
+    half_0 = anchor_0[1] / 2
+    halfs = anchors[:, 1] / 2
+
+    top_0 = anchor_0[0] - half_0
+    tops = anchors[:, 0] - halfs
+
+    bottom_0 = anchor_0[0] + half_0
+    bottoms = anchors[:, 0] + halfs
+
+    min_top = torch.min(top_0, tops)
+    max_top = torch.max(top_0, tops)
+
+    min_bottom = torch.min(bottom_0, bottoms)
+    max_bottom = torch.max(bottom_0, bottoms)
+
+    area_i = min_bottom - max_top
+    area_o = max_bottom - min_top
 
     return area_i / area_o
 
 
-if __name__ == "__main__":
-    x = torch.rand(1, 4, 3)
-    w = torch.rand(1, 4, 3, 2)
-    y = x > 0
-    # y = x.unsqueeze(3).expand(1, 4, 3, 2) > 0
+# def _iou(box_1, box_2):
+#     """Intersection Over Union
 
-    print(w)
-    print(y)
-    print(w[y].view(-1, 2))
-    # print(y > 0)
+#     Input:
+#         box_1 -- a sequence like [x_0, y_0, x_1, y_1], representing top-left and bottom-right corners of a box
+#         box_2 -- same as box_1
+#     """
+
+#     min_x_0 = min(box_1[0], box_2[0])
+#     min_y_0 = min(box_1[1], box_2[1])
+#     min_x_1 = min(box_1[2], box_2[2])
+#     min_y_1 = min(box_1[3], box_2[3])
+
+#     max_x_0 = max(box_1[0], box_2[0])
+#     max_y_0 = max(box_1[1], box_2[1])
+#     max_x_1 = max(box_1[2], box_2[2])
+#     max_y_1 = max(box_1[3], box_2[3])
+
+#     area_i = (min_x_1 - max_x_0) * (min_y_1 - max_y_0)
+#     area_o = (max_x_1 - min_x_0) * (max_y_1 - min_y_0)
+
+#     return area_i / area_o
+
+
+if __name__ == "__main__":
+    x = torch.randn(2, 3)
+    y = torch.argsort(x)
+
+    print(x)
